bb1c4dea0142c7fa801049b40896b4b0fb2b761e
double-joker <143305077+double-joker@users.noreply.github.com>
2025-01-10 11:45:00 +0800

    Update config.js

--- birth-want-to-say/config.js
+++ birth-want-to-say/config.js
@@ -1,4 +1,4 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+// 
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
@@ -9,16 +9,16 @@ var config = {
         "这是我认识你的",
         "第一个生日！",
         "这段时间以来，我见过你",
-        "奇怪的样子",
-        "可爱的样子",
-        "生气的样子",
-        "都是我喜欢的样子",
+        "毕业的样子",
+        "一本正经的样子",
+        "吃饭都不忘摆拍的样子",
+        "都是独一无二的样子",
         "对了，礼物收全了吗？",
-        "今年要吃好好的",
-        "喂饱饱的",
-        "然后才有力气干活",
-        "和想我！",
-        "生日快乐~~",
+        "今年要吃好好的！",
+        "耍好好的！",
+        "然后才能更好的练琵琶",
+        "和~",
+        "再次祝你生日快乐~~",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -34,32 +34,32 @@ var config = {
         "送礼物": "./imgs/3.jpg",
         "今天是你的生日": "./imgs/shengri.jpeg",
         "这是我认识你的": "./imgs/4.png",
-        "第一个生日！": "./imgs/.jpg",
-        "这两年来，我见过你": "./imgs/yangzi.jpeg",
-        "奇怪的样子": "./imgs/nothing.jpg",
-        "可爱的样子": "./imgs/nothing.jpg",
-        "生气的样子": "./imgs/nothing.jpg",
-        "都是我喜欢的样子": "./imgs/nothing.jpg",
+        "第一个生日！": "./imgs/5.jpg",
+        "这段时间以来，我见过你": "./imgs/yangzi.jpeg",
+        "毕业的样子": "./imgs/6.jpg",
+        "一本正经的样子": "./imgs/7.jpg",
+        "吃饭都不忘摆拍的样子": "./imgs/8.jpg",
+        "都是独一无二的样子": "./imgs/9.jpg",
         "对了，礼物收全了吗？": "./imgs/chuo.gif",
-        "今年要吃好好的": "./imgs/chihaodian.jpeg",
-        "喂饱饱的": "./imgs/weibao.jpeg",
-        "然后才有力气干活": "./imgs/nothing.jpg",
-        "和想我！": "./imgs/nothing.jpg",
-        "生日快乐~~": "./imgs/nothing.jpg"
+        "今年要吃好好的！": "./imgs/chihaodian.jpeg",
+        "耍好好的！": "./imgs/10.jpg",
+        "然后才能更好的练琵琶": "./imgs/11.jpg",
+        "和~": "./imgs/12.jpg",
+        "再次祝你生日快乐~~": "./imgs/13.jpg"
     },
     
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "点我开始",
         play: "上音乐",
-        bannar_coming: "搞点颜色",
+        bannar_coming: "上点特色",
         balloons_flying: "好像还少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐～",
+        wish_message: "生日快乐呀～",
         story: "A MESSAGE FOR YOU"
     },
 
     // 结束语
-    loveText: '爱你的X先生~'
+    loveText: '送给李慧敏好友友！'
 };
